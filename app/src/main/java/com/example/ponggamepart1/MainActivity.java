89861f63399a2b9d93ae5345ab479ddacc592c85
Nawaphon Wichaidit <nawaphonwichaidit@users.noreply.github.com>
2020-10-23 15:19:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ponggamepart1/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.ponggamepart1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Point;
+import android.view.Display;
+import android.os.Bundle;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class MainActivity extends AppCompatActivity {
+    private PongGame mPongGame;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        mPongGame = new PongGame(this, size.x, size.y);
+        setContentView(mPongGame);
+
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mPongGame.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPongGame.pause();
+    }
+
+}
